c6335c54e9fc61e7e9fb2fc98a6256b889cec6c9
Zeynep <zeynepkaya_26@hotmail.com>
2022-02-01 21:42:57 +0000

    Do exercise 1 and 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find 1
+@store2 = Store.find 2
+
+@store1.name = 'Zeyneps store'
+@store1.save
+
+puts @store1.name
