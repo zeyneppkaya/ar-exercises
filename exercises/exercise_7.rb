ccb32d7edb9f84edfc86d771a3a98ed500877f46
Zeynep <zeynepkaya_26@hotmail.com>
2022-02-01 22:35:51 +0000

    Add Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name:"
+
+@new_store_name = gets.chomp
+
+new_store = Store.new(name: @new_store_name, annual_revenue: 500000, mens_apparel: true, womens_apparel: false)
+
+new_store.save
+
+new_store.errors.messages.each do |message|
+  puts message
+end 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true }
+    validate :apparel
+  
+    def apparel
+      if mens_apparel.blank? && womens_apparel.blank?
+        errors.add(:mens_apparel, "at least one kind of apparel must be carried")
+      end
+    end
 end
